7f0bd2a80f6f197ec45579676c9052a38ae8e393
Alexandria <alexandria@example.com>
2026-07-18 16:07:14 -0400

    fix: remove unused imports

--- app/src/main/java/com/alexandria/app/update/UpdateChecker.kt
+++ app/src/main/java/com/alexandria/app/update/UpdateChecker.kt
@@ -2,8 +2,6 @@ package com.alexandria.app.update
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
-import android.os.Build
 import android.util.Log
 import androidx.core.content.FileProvider
 import kotlinx.coroutines.Dispatchers
